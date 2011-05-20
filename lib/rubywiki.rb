61f36cf66f2f630263a2f42b5400c363f0dabef3
Xiaomao Chen <xiaomao5@live.com>
2011-05-20 12:07:53 -0400

    Deleted code to add stuff to $: since it's gem's job.
    Signed-off-by: Xiaomao Chen <xiaomao5@live.com>

--- lib/rubywiki.rb
+++ lib/rubywiki.rb
@@ -6,15 +6,6 @@
 # Copyright:: SoftX Technologies Inc.
 # License:: GNU General Public License version 3
 
-ver = RUBY_VERSION.split('.')
-RubyMajorVersion = ver[0].to_i
-RubyMinorVersion = ver[1].to_i
-RubyBuildVersion = ver[2].to_i
-if RubyMajorVersion >= 1 and RubyMinorVersion >= 9 and RubyBuildVersion >= 2
-    $: << File::expand_path(File::dirname(__FILE__))
-    $: << File::expand_path(File::dirname(__FILE__) + '/rubywiki')
-end
-
 require 'rubygems'
 require 'yaml'
 require 'curb'
